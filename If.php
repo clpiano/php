53b6a083e198746bbdc5b8e74becd54ea3fdf9d5
clpiano <111512024+clpiano@users.noreply.github.com>
2022-12-28 23:25:32 +0900

    study

--- If.php
+++ If.php
@@ -1,11 +1,17 @@
 <?php
 //if文
 //==で等しい、!=で等しくない
+//elseif if文が実行されなかった場合(つなげてOK)
+// \nで改行
 
-    $name = trim(fgets(STDIN));
+    $name = trim(fgets(STDIN));  //標準入力された文字によって分岐
     echo "Hello " . $name . "\n";
 
     if ($name == "PHP") {
         echo "Welcome\n";
+    } elseif ($name == "php") {
+        echo "Good morning\n";
+    } else {
+        echo "Goodbye\n";
     }
 ?>
